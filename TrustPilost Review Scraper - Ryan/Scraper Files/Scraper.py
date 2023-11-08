95fd67a27bd46276536a0974d1eb3350a12f131f
Syed Shahbaz Ali Shah <62151454+syedshahbazali-daata@users.noreply.github.com>
2023-11-08 09:01:34 -0800

    Add files via upload

---
+++ TrustPilost Review Scraper - Ryan/Scraper Files/Scraper.py
@@ -0,0 +1,149 @@
+import requests
+import json
+from datetime import datetime, timedelta
+import sys
+
+# Enter the url and no of days as arguments
+try:
+    enter_the_url = str(sys.argv[1])
+    no_of_days = int(sys.argv[2])
+except:
+    print("Please enter the url and no of days as arguments "
+          "Example: python script.py https://www.trustpilot.com/review/super.com 30")
+    sys.exit(1)
+
+
+
+def check_url_code():
+    for i in range(1,9):
+        try:
+            headers = {
+                "authority": "www.trustpilot.com",
+                "accept": "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "referer": "https://www.trustpilot.com/review/super.com?businessunit=super.com&page=1&sort=recency",
+                "sec-ch-ua-mobile": "?0",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/"
+                              "537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+                "x-nextjs-data": "1"
+            }
+
+            # Getting the data from the url
+            requests.request("GET", f"https://www.trustpilot.com/_next/data/businessunitprofile-consumersite-81{i}6/review/panaceafinancial.com.json?page=2&businessUnit=panaceafinancial.com", headers=headers).json()
+
+
+            return i
+        except:
+            pass
+
+    return None
+
+# Function to calculate the days difference
+def days_until_date(input_date):
+    date_format = "%m/%d/%Y"
+
+    try:
+        input_datetime = datetime.strptime(input_date, date_format)
+        current_datetime = datetime.now()
+        time_difference = current_datetime - input_datetime
+        days_difference = time_difference.days
+
+        return days_difference
+    except ValueError:
+        return -1
+
+
+# Function to get the business unit
+page = 1
+try:
+    business_unit = enter_the_url.split('/')[-1].split('?')[0]
+except:
+    business_unit = enter_the_url.split('/')[-1]
+
+url_secure_code = check_url_code()
+url = f'https://www.trustpilot.com/_next/data/businessunitprofile-consumersite-81{url_secure_code}6/review/{business_unit}.json'
+
+
+
+
+all_reviews_data = []
+
+keep_running = True
+print("Please wait while we are fetching the data\n")
+
+# Loop to get the data from all the pages
+while keep_running:
+
+    if page != 1:
+        querystring = {"page": f"{page}", "sort": "recency", "businessUnit": f"{business_unit}"}
+    else:
+        querystring = {"sort": "recency", "businessUnit": f"{business_unit}"}
+    payload = ""
+    headers = {
+        "authority": "www.trustpilot.com",
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "referer": "https://www.trustpilot.com/review/super.com?businessunit=super.com&page=1&sort=recency",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/"
+                      "537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        "x-nextjs-data": "1"
+    }
+
+    # Getting the data from the url
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    json_data = response.json()
+
+    # Getting the data from the json object
+
+    reviews_object = json_data.get("pageProps", {}).get("reviews", [])
+    for review in reviews_object:
+        user_name = review.get("consumer", {}).get("displayName")
+        location = review.get("location")
+        stars = review.get("rating")
+        user_id = review.get("id")
+        source_url = f"https://www.trustpilot.com/review/{user_id}"
+        title = review.get("title")
+        details = review.get("text")
+        try:
+            date = review.get("dates", {}).get("publishedDate")
+            date = str(date).split('T')[0]
+            date = datetime.strptime(date, "%Y-%m-%d").strftime("%m/%d/%Y")
+        except:
+            date = review.get("dates", {}).get("publishedDate")
+
+        # Creating the data object
+        data = {
+            "User": user_name,
+            "Location": location,
+            "Stars": stars,
+            "Date": date,
+            "Source_url": source_url,
+            "id": user_id,
+            "Title": title,
+            "Details": details
+        }
+
+        # Checking the days difference
+        days_difference = days_until_date(date)
+
+        if days_difference <= no_of_days:
+            all_reviews_data.append(data)
+        else:
+            keep_running = False
+            break
+    print(f"Page {page} has been scraped")
+    page += 1
+
+# Saving the data in json file
+with open('Review_Scraper.json', 'w') as outfile:
+    json.dump(all_reviews_data, outfile, indent=4)
+
+# Printing the message
+print("Data has been Scraped and saved")
